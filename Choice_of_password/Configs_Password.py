43ded2b11db26612d770e533524e058e2849bf76
Vinicius Mendes <vmendes2002@outlook.com>
2024-09-07 14:59:12 -0300

    Alteração de nome da variável

--- Choice_of_password/Configs_Password.py
+++ Choice_of_password/Configs_Password.py
@@ -1,10 +1,10 @@
 def configs_password():
-    comprimento = int(input("Digite o comprimento da senha: "))
-    if not comprimento >= 5:
+    comprimento_senha = int(input("Digite o comprimento da senha: "))
+    if not comprimento_senha >= 5:
         raise ValueError("escolha o comprimento maior que 5, para senha mais segura!")
-    usar_maiusculas = input("Deseja incluir letras maiúsculas? (s/n) ").lower() == 's'
-    usar_minusculas = input("Deseja incluir letras minúsculas? (s/n) ").lower() == 's'
-    usar_numeros = input("Deseja incluir números? (s/n) ").lower() == 's'
-    usar_especiais = input("Deseja incluir caracteres especiais? (s/n) ").lower() == 's'
+    usar_senha_maiusculas = input("Deseja incluir letras maiúsculas? (s/n) ").lower() == 's'
+    usar_senha_minusculas = input("Deseja incluir letras minúsculas? (s/n) ").lower() == 's'
+    usar_senha_numeros = input("Deseja incluir números? (s/n) ").lower() == 's'
+    usar_senha_especiais = input("Deseja incluir caracteres especiais? (s/n) ").lower() == 's'
 
-    return comprimento, usar_maiusculas, usar_minusculas, usar_numeros, usar_especiais
+    return comprimento_senha, usar_senha_maiusculas, usar_senha_minusculas, usar_senha_numeros, usar_senha_especiais
